448547c9585c6b3f9ab1d44a63c09c9036bdf352
Anmengfei <mf_0604@163.com>
2020-09-28 21:07:05 +0800

    我发起的模块

--- pages/myfaqi/index.js
+++ pages/myfaqi/index.js
@@ -38,26 +38,53 @@ Page({
     var arr = [
       {
         id: 1,
-        description: '未来两年，在线教育的市场规模仍有增长趋势。总体来说：对于K12教育市场，随着在线教育产品的效果及口碑逐渐提升，加上2020年上半年疫情促使在线教育常态化、市场刚需等原因，未来K12在线教育市场还大有可为。',
-        time: '2020-9-27 18:20:20',
-        type:'深基坑',
-        status: '未处理'
+        construction_site_name: '项目A',
+        from_user: '张三',
+        to_user: '李四',
+        cc_people: '王五',
+        context: '未来两年，在线教育的市场规模仍有增长趋势。总体来说：对于K12教育市场，随着在线教育产品的效果及口碑逐渐提升，加上2020年上半年疫情促使在线教育常态化、市场刚需等原因，未来K12在线教育市场还大有可为。',
+        risk_level: 1,
+        process_status: 1,
+        start_time: '2020-9-27 18:20:20',
+        set_end_time: '2020-9-27 18:20:20',
+        handing_time: '2020-9-27 18:20:20',
+        check_type:'安全',
+        check_type_offspring: '深基坑',
+        flag: 1
       },
 
       {
-        id: 2,
-        description: '未来两年，在线教育的市场规模仍有增长趋势。总体来说：对于K12教育市场，随着在线教育产品的效果及口碑逐渐提升，加上2020年上半年疫情促使在线教育常态化、市场刚需等原因，未来K12在线教育市场还大有可为。',
-        time: '2020-9-27 18:20:20',
-        type:'塔吊',
-        status: '处理中'
+        id: 1,
+        construction_site_name: '项目A',
+        from_user: '张三',
+        to_user: '李四',
+        cc_people: '王五',
+        context: '未来两年，在线教育的市场规模仍有增长趋势。总体来说：对于K12教育市场，随着在线教育产品的效果及口碑逐渐提升，加上2020年上半年疫情促使在线教育常态化、市场刚需等原因，未来K12在线教育市场还大有可为。',
+        risk_level: 2,
+        process_status: 2,
+        start_time: '2020-9-27 18:20:20',
+        set_end_time: '2020-9-27 18:20:20',
+        handing_time: '2020-9-27 18:20:20',
+        check_type:'安全',
+        check_type_offspring: '深基坑',
+        flag: 1
       },
 
       {
-        id: 3,
-        description: '未来两年，在线教育的市场规模仍有增长趋势。总体来说：对于K12教育市场，随着在线教育产品的效果及口碑逐渐提升，加上2020年上半年疫情促使在线教育常态化、市场刚需等原因，未来K12在线教育市场还大有可为。',
-        time: '2020-9-27 18:20:20',
-        type: '升降机',
-        status: '已处理'
+        id: 1,
+        construction_site_name: '项目A',
+        from_user: '张三',
+        to_user: '李四',
+        cc_people: '王五',
+        context: '未来两年，在线教育的市场规模仍有增长趋势。总体来说：对于K12教育市场，随着在线教育产品的效果及口碑逐渐提升，加上2020年上半年疫情促使在线教育常态化、市场刚需等原因，未来K12在线教育市场还大有可为。',
+        risk_level: 3,
+        process_status: 3,
+        start_time: '2020-9-27 18:20:20',
+        set_end_time: '2020-9-27 18:20:20',
+        handing_time: '2020-9-27 18:20:20',
+        check_type:'安全',
+        check_type_offspring: '深基坑',
+        flag: 1
       }
     ]
 
@@ -148,6 +175,21 @@ Page({
       myFaqiList: arr
     })
 
+  },
+
+  parseWenzi(num) {
+    if(num === 1) {
+      return '一级'
+    } 
+    if(num === 2) {
+      return '二级'
+    } 
+    if(num === 3) {
+      return '三级'
+    } 
+    if(num === 4) {
+      return '四级'
+    } 
   }
 
   
