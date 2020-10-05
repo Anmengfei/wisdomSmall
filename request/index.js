d0eb6dafb7da3f4942e2c4cf10f2ced431786dfa
Anmengfei <mf_0604@163.com>
2020-10-05 09:29:11 +0800

    发起详情

--- request/index.js
+++ request/index.js
@@ -19,7 +19,7 @@ export const request=(params)=>{
     
 
   // 定义公共的url
-  const baseUrl="https://www.zytb.top/show/cee";
+  const baseUrl="http://121.36.106.18:38088/";
   return new Promise((resolve,reject)=>{
     wx.request({
      ...params,
